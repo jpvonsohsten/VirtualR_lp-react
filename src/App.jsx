3c68821a36b570f020442a75ed3e96cb1df16a46
João Pedro Von Sohsten Almeida Magalhães <joao.magalhaes@elecnor.com.br>
2024-07-31 10:27:27 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import HeroSection from "./components/HeroSection"
+import Navbar from "./components/Navbar"
+import FeatureSection from "./components/FeatureSection"
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+      </div>
+      
+  </>
+  )
+}
+
+export default App
